a46c0962a43dba574dcb78d7fcebe4db3aa8767b
Nikolai <pythondevbg8@abv.bg>
2022-09-15 17:44:11 +0300

    DTOs

---
+++ dsharp-bot-project/Commands/MiscellaneousCommands/DTOs/DogImageModel.cs
@@ -0,0 +1,10 @@
+﻿namespace dsharp_bot_project.Commands.MiscellaneousCommands.DTOs
+{
+    using Newtonsoft.Json;
+
+    public class DogImageModel
+    {
+        [JsonProperty("message")]
+        public string Image { get; set; }
+    }
+}
