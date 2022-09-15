04fc8a9dc24e38c134c4852dd4debc16ddf5333e
Nikolai <pythondevbg8@abv.bg>
2022-09-15 17:44:57 +0300

    Module that handles miscellaneous commands

---
+++ dsharp-bot-project/Modules/MiscellaneousModule.cs
@@ -0,0 +1,19 @@
+﻿namespace dsharp_bot_project.Modules
+{
+    using DSharpPlus.CommandsNext;
+    using DSharpPlus.CommandsNext.Attributes;
+    using dsharp_bot_project.Commands.MiscellaneousCommands;
+
+    public class MiscellaneousModule : BaseCommandModule
+    {
+
+        [Command("showdogimage")]
+        [Cooldown(
+            GlobalConstants.DOG_COMMAND_MAX_USES,
+            GlobalConstants.DOG_COMMAND_RESET_AFTER_ONE_HOUR,
+            CooldownBucketType.User)]
+        public async Task ShowDogImage(CommandContext ctx)
+            => await ShowDogImageCommand.ShowDogImage(ctx);
+
+    }
+}
