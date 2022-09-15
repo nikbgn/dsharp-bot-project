19e452acf9e6c8eb8bedf78ac0a9da0aa5595b1c
Nikolai <pythondevbg8@abv.bg>
2022-09-15 17:43:20 +0300

    Command that says welcome

---
+++ dsharp-bot-project/Commands/GreetingCommands/SayWelcomeCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace dsharp_bot_project.Commands.GreetingCommands
+{
+    using DSharpPlus.CommandsNext;
+
+    public class SayWelcomeCommand
+    {
+        public static async Task SayWelcome(CommandContext ctx)
+            => await ctx.RespondAsync("Welcome and thank you for executing this command!");
+    }
+}
