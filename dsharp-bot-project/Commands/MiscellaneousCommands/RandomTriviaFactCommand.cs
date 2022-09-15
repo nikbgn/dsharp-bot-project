7e005f37c905e87db024f239ca031746c55f3337
Nikolai <pythondevbg8@abv.bg>
2022-09-15 19:47:31 +0300

    A command that gives random trivia facts

---
+++ dsharp-bot-project/Commands/MiscellaneousCommands/RandomTriviaFactCommand.cs
@@ -0,0 +1,30 @@
+﻿namespace dsharp_bot_project.Commands.MiscellaneousCommands
+{
+    using DSharpPlus.CommandsNext;
+
+    public class RandomTriviaFactCommand
+    {
+        private static string triviaFactsApiURL = "http://numbersapi.com/random/trivia";
+
+        public static async Task ShowTriviaFact(CommandContext ctx)
+        {
+            string triviaFact = await GetTriviaFactAsync();
+            string message = !string.IsNullOrEmpty(triviaFact) ?
+                string.Format(GlobalConstants.TRIVIA_MESSAGE_SUCCESS, new string[] {triviaFact,triviaFactsApiURL}) :
+                GlobalConstants.MESSAGE_ERROR;
+            await ctx.RespondAsync(message);
+        }
+
+        public static async Task<string> GetTriviaFactAsync()
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(triviaFactsApiURL);
+            if (response.IsSuccessStatusCode)
+            {
+                string fact = await response.Content.ReadAsStringAsync();
+                return fact;
+            }
+            return null;
+        }
+    }
+}
