586e73cd0a5ece72065ff075ca04776543808073
Nikolai <pythondevbg8@abv.bg>
2022-09-15 19:48:44 +0300

    More messages and settings added

--- dsharp-bot-project/GlobalConstants.cs
+++ dsharp-bot-project/GlobalConstants.cs
@@ -4,11 +4,26 @@
     {
         //Discord bot token goes here.
         public const string DISCORD_TOKEN = "BOT_TOKEN_GOES_HERE";
-
+        
 
 
         //Dog image command settings.
         public const int DOG_COMMAND_MAX_USES = 3;
         public const int DOG_COMMAND_RESET_AFTER_ONE_HOUR = 3600;
+        public static string DOG_MESSAGE_SUCCESS = 
+            $"**Note: You can use this command only {DOG_COMMAND_MAX_USES} times per hour!**\n" +
+            "The images are provided by \"{0}\"\n" +
+            "{1}";
+
+        //Trivia command settings
+        public const int TRIVIA_COMMAND_MAX_USES = 3;
+        public const int TRIVIA_COMMAND_RESET_AFTER_ONE_HOUR = 3600;
+        public static string TRIVIA_MESSAGE_SUCCESS = 
+            $"**Note: You can use this command only {TRIVIA_COMMAND_MAX_USES} times per hour!**\n" +
+            "Did you know that: {0}\n" +
+            "This fact was provided by {1}";
+
+        //Common command messages
+        public const string MESSAGE_ERROR = "There was an error, please try again later...";
     }
 }
