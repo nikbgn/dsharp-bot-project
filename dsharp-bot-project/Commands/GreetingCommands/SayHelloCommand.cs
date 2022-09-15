84b690db2983838766ad90f6a69fd55608abdc22
Nikolai <pythondevbg8@abv.bg>
2022-09-15 17:43:10 +0300

    Command that says hello

---
+++ dsharp-bot-project/Commands/GreetingCommands/SayHelloCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace dsharp_bot_project.Commands.GreetingCommands
+{
+    using DSharpPlus.CommandsNext;
+
+    public static class SayHelloCommand
+    {
+        public static async Task SayHello(CommandContext ctx)
+            => await ctx.RespondAsync("Hello there!");
+    }
+}
