d2929c32d7034a36e4ae307bf58e24c9ed6f81b8
Nikolai <pythondevbg8@abv.bg>
2022-09-15 14:32:01 +0300

    Greeting Module that holds some greeting commands for testing purposes

---
+++ dsharp-bot-project/Commands/GreetingModule.cs
@@ -0,0 +1,16 @@
+﻿namespace dsharp_bot_project.Commands
+{
+    using DSharpPlus.CommandsNext;
+    using DSharpPlus.CommandsNext.Attributes;
+
+    public class GreetingModule : BaseCommandModule
+    {
+        [Command("sayhello")]
+        public async Task SayHello(CommandContext ctx)
+            => await ctx.RespondAsync("Hello there!");
+
+        [Command("saywelcome")]
+        public async Task SayWelcome(CommandContext ctx)
+            => await ctx.RespondAsync("Welcome and thank you for executing this command!");
+    }
+}
